1c928562c6746d9abc9f08d15d50f22be37c8b18
Haneet Arya <haneetarya16@gmail.com>
2021-07-24 22:19:30 +0530

    Final Commit 10:19pm

--- app/src/main/java/com/example/adg_vit_final/Login.java
+++ app/src/main/java/com/example/adg_vit_final/Login.java
@@ -2,9 +2,13 @@ package com.example.adg_vit_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private TextView btnNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,14 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+        btnNewAccount = findViewById(R.id.btn_new_account);
+
+        btnNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Signup1.class));
+            }
+        });
 
     }
 }
