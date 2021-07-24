1bbc864251796c6a84b90d446e7b7decbf328bdf
raehat <raehatsinghnanda@gmail.com>
2021-07-24 21:55:56 +0530

    final commit, Raehat Singh Nanda. 9:55pm 24/7/21

--- app/src/main/java/com/example/adg_vit_final/FirstBoarding.java
+++ app/src/main/java/com/example/adg_vit_final/FirstBoarding.java
@@ -2,6 +2,7 @@ package com.example.adg_vit_final;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
