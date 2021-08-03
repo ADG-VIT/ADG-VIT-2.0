f12aad06ca1974ed12dc14aa69f47b1e03784287
Haneet-Arya <haneetarya16@gmail.com>
2021-08-03 18:09:48 +0530

    Project API

--- app/src/main/java/com/example/adg_vit_final/NetworkInterface/NetworkAPI.java
+++ app/src/main/java/com/example/adg_vit_final/NetworkInterface/NetworkAPI.java
@@ -1,6 +1,7 @@
 package com.example.adg_vit_final.NetworkInterface;
 
 import com.example.adg_vit_final.NetworkModels.EventModelNetwork;
+import com.example.adg_vit_final.NetworkModels.ProjectModelNetwork;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ import retrofit2.http.Path;
 public interface NetworkAPI {
     @GET("{param}")
     Call<List<EventModelNetwork>> getEvents(@Path("param") String param);
+
+    @GET("projects/1")
+    Call<List<ProjectModelNetwork>> getProjects();
 }
