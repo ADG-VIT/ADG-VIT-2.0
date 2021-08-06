f3d84242b56fb432f1a49f3d9d28ec7ebaa8ce45
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2021-08-06 22:15:55 +0530

    Test branching

--- app/src/main/java/com/example/adg_vit_final/JavaActivities/Domains.java
+++ app/src/main/java/com/example/adg_vit_final/JavaActivities/Domains.java
@@ -4,16 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.adg_vit_final.DataModels.DomainsDataModel;
 import com.example.adg_vit_final.R;
-import com.example.adg_vit_final.RecyclerViewAdapter.DomainsAdapter;
-import com.example.adg_vit_final.RecyclerViewAdapter.EventsAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
