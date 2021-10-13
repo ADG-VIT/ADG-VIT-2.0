24f943d8472c732e9c837a36bcfcb82b9255ebcc
Jatin7385 <jats7385@gmail.com>
2021-10-13 11:52:33 +0530

    Sign Up Fixed

--- app/src/main/java/com/example/adg_vit_final/JavaActivities/Signup4.java
+++ app/src/main/java/com/example/adg_vit_final/JavaActivities/Signup4.java
@@ -3,6 +3,7 @@ package com.example.adg_vit_final.JavaActivities;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,14 +14,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.adg_vit_final.NetworkModels.SignUpCallBack;
 import com.example.adg_vit_final.NetworkModels.User;
 import com.example.adg_vit_final.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.jetbrains.annotations.NotNull;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 import static com.example.adg_vit_final.NetworkUtil.NetworkUtils.networkAPI;
+
+import java.lang.reflect.Type;
 
 public class Signup4 extends AppCompatActivity {
     Button createAccountBtn;
@@ -50,12 +56,6 @@ public class Signup4 extends AppCompatActivity {
             public void onClick(View view) {
                 progressDialog.show();
                 user.setRegno(usrRegNo.getText().toString());
-//                System.out.println("Name : " + user.getName());
-//                System.out.println("Email : " + user.getEmail());
-//                System.out.println("PhNumber : " + user.getPhone());
-//                System.out.println("Password : " + user.getPassword());
-//                System.out.println("Univ : " + user.getUniversity());
-//                System.out.println("Reg no : " + user.getRegno());
                 registerUser();
 
             }
@@ -70,27 +70,35 @@ public class Signup4 extends AppCompatActivity {
             @Override
             public void onResponse(@NotNull Call<SignUpCallBack> call, @NotNull Response<SignUpCallBack> response) {
 
-                if(response.isSuccessful())
-                {
-                    try {
-                        SignUpCallBack responseUser = response.body();
-                        String message = responseUser.getMessage();
-                        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
-                        Intent intent1 = new Intent(getApplicationContext(),Login.class);
+                try {
+                    Log.i("responseCode",String.valueOf(response.code()));
+                    SignUpCallBack callBack=response.body();
+                    ResponseBody error = response.errorBody();
+                    if (response.code()==406){
+                        Gson gson = new Gson();
+                        Type type = new TypeToken<SignUpCallBack>() {}.getType();
+                        SignUpCallBack errorResponse = gson.fromJson(response.errorBody().charStream(),type);
+                        Log.i("message", errorResponse.getMessage());
+                        Toast.makeText(getApplicationContext(),/*"Account Creation failed. Please try again!"*/"Sign Up Failed : " + errorResponse.getMessage(),Toast.LENGTH_LONG).show();
                         progressDialog.dismiss();
-                        startActivity(intent1);
-                    }catch (Exception e){
-                        Toast.makeText(getApplicationContext(),"SignUp failed. Please try again!",Toast.LENGTH_LONG).show();
-                        Intent intent1 = new Intent(getApplicationContext(),Login.class);
-                        progressDialog.dismiss();
-                        startActivity(intent1);
                     }
-                }else {
-                    SignUpCallBack responseUser = response.body();
-                    String message = responseUser.getMessage();
-                    Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
-                    progressDialog.dismiss();
-                    return;
+                    else {
+                        try {
+                            SignUpCallBack signupresp = response.body();
+                            String message = signupresp.getMessage();
+                            String token = signupresp.getToken();
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                            Intent intent1 = new Intent(getApplicationContext(),Login.class);
+                            progressDialog.dismiss();
+                            startActivity(intent1);
+                        }catch (Exception e){
+                            Toast.makeText(getApplicationContext(),"SignUp failed. Please try again!",Toast.LENGTH_LONG).show();
+                            //System.out.println(e.getLocalizedMessage());
+                            progressDialog.dismiss();
+
+                        }}}catch (Exception e){
+                    Log.i("hello", e.getMessage());
+                    Toast.makeText(Signup4.this, ""+e.getMessage(), Toast.LENGTH_LONG).show();
                 }
 
 
